38209f2c4b3c34b2a4bf79f07f9173a7daa71a6c
TRPBall <trpball@hotmail.com>
2016-10-09 15:23:47 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/app/trp/btle_bike_rc/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.app.trp.btle_bike_rc;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
